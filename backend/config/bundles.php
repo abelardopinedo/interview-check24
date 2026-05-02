57f12e2637f8bf2f8db665fa70005b0df676ace3
abelardo <abelardo@talent.com>
2026-05-03 00:51:18 +0200

    - Fixed issues in docker

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,5 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
